63cc7840e8ddf2015246ec2f56b1105ad414692f
Mochi <master.c.mochi@gmail.com>
2016-12-05 16:57:38 +0900

    kernel（無限hlt）追加

---
+++ src/kernel/include/cpu/IA32/IA32Descriptor.h
@@ -0,0 +1,136 @@
+/******************************************************************************/
+/* src/kernel/include/cpu/IA32/IA32Descriptors.h                              */
+/*                                                                 2016/12/05 */
+/* Copyright (C) 2016 Mochi                                                   */
+/******************************************************************************/
+#ifndef IA32_DESCRIPTOR_H
+#define IA32_DESCRIPTOR_H
+/******************************************************************************/
+/* インクルード                                                               */
+/******************************************************************************/
+#include <stdint.h>
+
+
+/******************************************************************************/
+/* 定義                                                                       */
+/******************************************************************************/
+/* ディスクリプタ存在フラグ */
+#define IA32_DESCRIPTOR_P_NO             ( 0 )  /**< ディスクリプタ無し */
+#define IA32_DESCRIPTOR_P_YES            ( 1 )  /**< ディスクリプタ有り */
+
+/* ディスクリプタ特権レベル */
+#define IA32_DESCRIPTOR_DPL_0            ( 0 )  /**< レベル0 */
+#define IA32_DESCRIPTOR_DPL_1            ( 1 )  /**< レベル1 */
+#define IA32_DESCRIPTOR_DPL_2            ( 2 )  /**< レベル2 */
+#define IA32_DESCRIPTOR_DPL_3            ( 3 )  /**< レベル3 */
+
+/* システムフラグ */
+#define IA32_DESCRIPTOR_S_YES            ( 0 )  /**< システムディスクリプタ   */
+#define IA32_DESCRIPTOR_S_NO             ( 1 )  /**< 非システムディスクリプタ */
+
+/* システムディスクリプタタイプ */
+#define IA32_DESCRIPTOR_TYPE_TSS16       ( 1 )  /**< 16ビットTSS            */
+#define IA32_DESCRIPTOR_TYPE_LDT         ( 2 )  /**< LDT                    */
+#define IA32_DESCRIPTOR_TYPE_TSS16_BUSY  ( 3 )  /**< 16ビットTSS(ビジー)    */
+#define IA32_DESCRIPTOR_TYPE_GATE16_CALL ( 4 )  /**< 16ビットコールゲート   */
+#define IA32_DESCRIPTOR_TYPE_GATE_TASK   ( 5 )  /**< タスクゲート           */
+#define IA32_DESCRIPTOR_TYPE_GATE16_INT  ( 6 )  /**< 16ビット割込みゲート   */
+#define IA32_DESCRIPTOR_TYPE_GATE16_TRAP ( 7 )  /**< 16ビットトラップゲート */
+#define IA32_DESCRIPTOR_TYPE_TSS32       ( 9 )  /**< 32ビットTSS            */
+#define IA32_DESCRIPTOR_TYPE_TSS32_BUSY  ( 11 ) /**< 32ビットTSS(ビジー)    */
+#define IA32_DESCRIPTOR_TYPE_GATE32_CALL ( 12 ) /**< 32ビットコールゲート   */
+#define IA32_DESCRIPTOR_TYPE_GATE32_INT  ( 14 ) /**< 32ビット割込みゲート   */
+#define IA32_DESCRIPTOR_TYPE_GATE32_TRAP ( 15 ) /**< 32ビットトラップゲート */
+
+/* 非システムディスクリプタタイプ */
+#define IA32_DESCRIPTOR_TYPE_DATA_R      ( 0 )  /**< データ 読取専用                                 */
+#define IA32_DESCRIPTOR_TYPE_DATA_A      ( 1 )  /**< データ 読取専用, アクセス済                     */
+#define IA32_DESCRIPTOR_TYPE_DATA_RW     ( 2 )  /**< データ 読書                                     */
+#define IA32_DESCRIPTOR_TYPE_DATA_RWA    ( 3 )  /**< データ 読書, アクセス済                         */
+#define IA32_DESCRIPTOR_TYPE_DATA_ER     ( 4 )  /**< データ エクスパンドダウン, 読取専用             */
+#define IA32_DESCRIPTOR_TYPE_DATA_EA     ( 5 )  /**< データ エクスパンドダウン, 読取専用, アクセス済 */
+#define IA32_DESCRIPTOR_TYPE_DATA_ERW    ( 6 )  /**< データ エクスパンドダウン, 読書                 */
+#define IA32_DESCRIPTOR_TYPE_DATA_ERWA   ( 7 )  /**< データ エクスパンドダウン, 読書, アクセス済     */
+#define IA32_DESCRIPTOR_TYPE_CODE_X      ( 8 )  /**< コード 実行専用                                 */
+#define IA32_DESCRIPTOR_TYPE_CODE_XA     ( 9 )  /**< コード 実行専用, アクセス済                     */
+#define IA32_DESCRIPTOR_TYPE_CODE_XR     ( 10 ) /**< コード 実行読取                                 */
+#define IA32_DESCRIPTOR_TYPE_CODE_XRA    ( 11 ) /**< コード 実行読取, アクセス済                     */
+#define IA32_DESCRIPTOR_TYPE_CODE_CX     ( 12 ) /**< コード コンフォーミング, 実行専用               */
+#define IA32_DESCRIPTOR_TYPE_CODE_CXA    ( 13 ) /**< コード コンフォーミング, 実行専用, アクセス済   */
+#define IA32_DESCRIPTOR_TYPE_CODE_CXR    ( 14 ) /**< コード コンフォーミング, 実行読取               */
+#define IA32_DESCRIPTOR_TYPE_CODE_CXRA   ( 15 ) /**< コード コンフォーミング, 実行読取, アクセス済   */
+
+/* リミット粒度 */
+#define IA32_DESCRIPTOR_G_BYTE           ( 0 )  /**< 1byte単位     */
+#define IA32_DESCRIPTOR_G_4K             ( 1 )  /**< 4,095byte単位 */
+
+/* デフォルトオペレーションサイズ */
+#define IA32_DESCRIPTOR_DB_16            ( 0 )  /**< 16bitビット */
+#define IA32_DESCRIPTOR_DB_32            ( 1 )  /**< 32bitビット */
+
+/* システム利用可能フラグ */
+#define IA32_DESCRIPTOR_AVL_OFF          ( 0 )  /**< 0 */
+#define IA32_DESCRIPTOR_AVL_ON           ( 1 )  /**< 1 */
+
+/* セグメントディスクリプタ */
+typedef struct {
+    uint16_t limit_low;         /* セグメントリミット(0-15)        */
+    uint16_t base_low;          /* セグメントベースアドレス(0-15)  */
+    uint8_t  base_middle;       /* セグメントベースアドレス(16-23) */
+    uint8_t  attr_type: 4;      /* セグメントタイプ                */
+    uint8_t  attr_s   : 1;      /* システムフラグ                  */
+    uint8_t  attr_dpl : 2;      /* ディスクリプタ特権レベル        */
+    uint8_t  attr_p   : 1;      /* ディスクリプタ存在フラグ        */
+    uint8_t  limit_high   : 4;  /* セグメントリミット(16-19)       */
+    uint8_t  attr_avl     : 1;  /* 未使用                          */
+    uint8_t  attr_reserved: 1;  /* 予約(0)                         */
+    uint8_t  attr_opSize  : 1;  /* デフォルトオペレーションサイズ  */
+    uint8_t  attr_g       : 1;  /* リミット粒度                    */
+    uint8_t  base_high;         /* セグメントベースアドレス(24-31) */
+} __attribute__( ( aligned( 8 ) ) ) IA32DescriptorSeg_t;
+
+/* TSSディスクリプタ */
+typedef struct {
+    uint16_t limit_low;         /* セグメントリミット(0-15)        */
+    uint16_t base_low;          /* セグメントベースアドレス(0-15)  */
+    uint8_t  base_middle;       /* セグメントベースアドレス(16-23) */
+    uint8_t  attr_type: 4;      /* セグメントタイプ                */
+    uint8_t  attr_s   : 1;      /* システムフラグ                  */
+    uint8_t  attr_dpl : 2;      /* ディスクリプタ特権レベル        */
+    uint8_t  attr_p   : 1;      /* ディスクリプタ存在フラグ        */
+    uint8_t  limit_high   : 4;  /* セグメントリミット(16-19)       */
+    uint8_t  attr_avl     : 1;  /* 未使用                          */
+    uint8_t  attr_reserved: 2;  /* 予約(0)                         */
+    uint8_t  attr_g       : 1;  /* リミット粒度                    */
+    uint8_t  base_high;         /* セグメントベースアドレス(24-31) */
+} __attribute__( ( aligned( 8 ) ) ) IA32DescriptorTSS_t;
+
+/* ゲートディスクリプタ */
+typedef struct {
+    uint16_t offset_low;        /* オフセット(0-15)         */
+    uint16_t selector;          /* セグメントセレクタ       */
+    uint8_t  reserved: 5;       /* 予約(0)                  */
+    uint8_t  count   : 3;       /* 引数カウント             */
+    uint8_t  attr_type: 4;      /* ディスクリプタタイプ     */
+    uint8_t  attr_s   : 1;      /* システムフラグ(0)        */
+    uint8_t  attr_dpl : 2;      /* ディスクリプタ特権レベル */
+    uint8_t  attr_p   : 1;      /* ディスクリプタ存在フラグ */
+    uint16_t offset_high;       /* オフセット(16-31)        */
+} __attribute__( ( aligned( 8 ) ) ) IA32DescriptorGate_t;
+
+/* ディスクリプタ */
+typedef union {
+    IA32DescriptorSeg_t  seg;   /* セグメントディスクリプタ */
+    IA32DescriptorTSS_t  tss;   /* TSSディスクリプタ        */
+    IA32DescriptorGate_t gate;  /* ゲートディスクリプタ     */
+} IA32Descriptor_t;
+
+/* 疑似ディスクリプタ */
+typedef struct {
+    uint16_t         limit;     /* リミット       */
+    IA32Descriptor_t *pBase;    /* ベースアドレス */
+} __attribute__( ( packed ) ) IA32DescriptorPseudo_t;
+
+
+/******************************************************************************/
+#endif
---
+++ src/kernel/include/cpu/IA32/IA32TSS.h
@@ -0,0 +1,62 @@
+/******************************************************************************/
+/* src/kernel/include/cpu/IA32/IA32TSS.h                                      */
+/*                                                                 2016/12/05 */
+/* Copyright (C) 2016 Mochi                                                   */
+/******************************************************************************/
+#ifndef IA32_TSS_H
+#define IA32_TSS_H
+/******************************************************************************/
+/* インクルード                                                               */
+/******************************************************************************/
+#include <stdint.h>
+
+
+/******************************************************************************/
+/* 定義                                                                       */
+/******************************************************************************/
+/* TSS */
+typedef struct {
+    uint16_t preTaskLink;       /* 前のタスクへのリンク    */
+    uint16_t reserved1;         /* 予約                    */
+    uint32_t esp0;              /* ESP0                    */
+    uint16_t ss0;               /* SS0                     */
+    uint16_t reserved2;         /* 予約                    */
+    uint32_t esp1;              /* ESP1                    */
+    uint16_t ss1;               /* SS1                     */
+    uint16_t reserved3;         /* 予約                    */
+    uint32_t esp2;              /* ESP2                    */
+    uint16_t ss2;               /* SS2                     */
+    uint16_t reserved4;         /* 予約                    */
+    uint32_t cr3;               /* CR0                     */
+    uint32_t eip;               /* EIP                     */
+    uint32_t eflags;            /* ELAGS                   */
+    uint32_t eax;               /* eax                     */
+    uint32_t ecx;               /* ecx                     */
+    uint32_t edx;               /* edx                     */
+    uint32_t ebx;               /* ebx                     */
+    uint32_t esp;               /* esp                     */
+    uint32_t ebp;               /* ebp                     */
+    uint32_t esi;               /* esi                     */
+    uint32_t edi;               /* edi                     */
+    uint16_t es;                /* es                      */
+    uint16_t reserved5;         /* 予約                    */
+    uint16_t cs;                /* cs                      */
+    uint16_t reserved6;         /* 予約                    */
+    uint16_t ss;                /* ss                      */
+    uint16_t reserved7;         /* 予約                    */
+    uint16_t ds;                /* ds                      */
+    uint16_t reserved8;         /* 予約                    */
+    uint16_t fs;                /* fs                      */
+    uint16_t reserved9;         /* 予約                    */
+    uint16_t gs;                /* gs                      */
+    uint16_t reserved10;        /* 予約                    */
+    uint16_t ldt;               /* LDTセグメントセレクタ   */
+    uint16_t reserved11;        /* 予約                    */
+    uint16_t reserved12:15;     /* 予約                    */
+    uint16_t t:1;               /* デバッグトラップフラグ  */
+    uint16_t ioMapBase;         /* I/Oマップベースアドレス */
+} IA32TSS_t;
+
+
+/******************************************************************************/
+#endif
